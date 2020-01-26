f349509eb090215b77239791221950b69da6e99c
heyou <heyou_0423@163.com>
2020-01-26 23:25:03 +0800

    update

--- Java/src/main/java/java8/stream/streamdemo/Dish.java
+++ Java/src/main/java/java8/stream/streamdemo/Dish.java
@@ -13,10 +13,16 @@ public class Dish {
     private final String name;
     private final boolean vegetarian;
     private final int calories;
+    private final Type type;
 
-    public Dish(String name, boolean vegetarian, int calories) {
+    public Dish(String name, boolean vegetarian, int calories, Type type) {
         this.name = name;
         this.vegetarian = vegetarian;
         this.calories = calories;
+        this.type = type;
+    }
+
+    public enum Type{
+        MEAT,FISH,OTHER
     }
 }
