00463b42fe76797cdf740f63384bec53b0e0128c
heyou-macbook <heyou_0423@163.com>
2020-04-05 23:23:20 +0800

    update：optional相关api

--- Java/src/main/java/java8/optional/optionaldemo/OptionalApi.java
+++ Java/src/main/java/java8/optional/optionaldemo/OptionalApi.java
@@ -1,8 +1,5 @@
 package java8.optional.optionaldemo;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
-import java.util.Collections;
 import java.util.Optional;
 
 /**
@@ -12,6 +9,20 @@ import java.util.Optional;
 public class OptionalApi {
     public static void main(String[] args) {
         User user = new User();
+        Optional<User> userOptional = Optional.<User>empty();
+        Optional<User> optionalUser = Optional.of(new User());
+
+        System.out.println(optionalUser.get());
+
+        Optional<Object> objectOptional = Optional.ofNullable(null);
+
+        System.out.println(objectOptional);
+        objectOptional.orElseGet(User::new);
+        objectOptional.orElse(new User());
+        /*objectOptional.orElseThrow(RuntimeException::new);
+        objectOptional.orElseThrow(()->new RuntimeException("Not Fount Reference"));*/
+        // 是否有值
+        System.out.println(objectOptional.isPresent());
         Optional<User> optional = Optional.of(user);
         //optional.orElse(new User("heyou",1,24,180.0));
 
@@ -19,5 +30,19 @@ public class OptionalApi {
         optional.ifPresent(System.out::println);
         Optional<String> optional1 = optional.map((a) -> "key" + a);
         System.out.println(optional1.get());
+
+        getUserName(null);
+        getUserNameByOptional(null);
+    }
+
+    private static String getUserName(User user){
+        if(null == user){
+            return "unknown";
+        }
+        return user.getName();
+    }
+
+    private static String getUserNameByOptional(User user){
+        return Optional.ofNullable(user).map(User::getName).orElse("unknown");
     }
 }
