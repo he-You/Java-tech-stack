da4a710c0e0548ffe216c6a9607678be059c30c3
heyou-macbook <heyou_0423@163.com>
2020-08-10 22:38:19 +0800

    update:Java 并发RateLimiter 令牌桶限流

---
+++ Java/src/main/java/thread/RateLimiterDemo.java
@@ -0,0 +1,33 @@
+package thread;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2020/8/10 20:42
+ */
+public class RateLimiterDemo {
+    private static RateLimiter limiter = RateLimiter.create(2);
+
+    public static class Task implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println(System.currentTimeMillis());
+        }
+    }
+
+    public static void main(String[] args) {
+//        for (int i = 0; i < 50; i++) {
+//            limiter.acquire();
+//            new Thread(new Task()).start();
+//        }
+
+        for (int i = 0; i < 50; i++) {
+            if (!limiter.tryAcquire()) {
+                continue;
+            }
+            new Thread(new Task()).start();
+        }
+    }
+}
