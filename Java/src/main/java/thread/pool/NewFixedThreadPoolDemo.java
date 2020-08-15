1e447373f1d5c4c1f2be699bfdc012ce8c1ae887
heyou-macbook <heyou_0423@163.com>
2020-08-15 15:25:52 +0800

    update:线程池-newFixedThreadPool

---
+++ Java/src/main/java/thread/pool/NewFixedThreadPoolDemo.java
@@ -0,0 +1,34 @@
+package thread.pool;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 返回固定线程数的线程池
+ *
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2020/8/15 14:33
+ */
+public class NewFixedThreadPoolDemo {
+    public static class MyTask implements Runnable{
+        @Override
+        public void run() {
+            System.out.println(System.currentTimeMillis() + "Thread ID:"+Thread.currentThread().getId());
+            Thread.currentThread().getId();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(task);
+        }
+    }
+}
