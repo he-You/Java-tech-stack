44b8cf3fa30aa30b8e740ad85e470fe6f2195b5d
heyou-macbook <heyou_0423@163.com>
2020-10-01 22:22:29 +0800

    add:动态规划之找零钱实现

--- Algorithm/src/main/java/doc/dp/code/CoinChange.java
+++ Algorithm/src/main/java/doc/dp/code/CoinChange.java
@@ -8,42 +8,39 @@ package doc.dp.code;
  */
 public class CoinChange {
     public static int coinChange(int[] coins, int amount) {
-        if (amount <= 0) {
+        // 合法性盘
+        if (coins.length < 1) {
+            return -1;
+        }
+        if (amount < 1) {
             return 0;
         }
-        // 记录最小兑换次数
+        // 获取硬币的个数
+        int n = coins.length;
+        // 初始化记忆数组
         int[] f = new int[amount + 1];
-        //填充1个硬币能兑换到的金额
-        for (int i = 0; i < coins.length; i++) {
-            if (coins[i] <= amount) {
-                f[coins[i]] = 1;
-            }
-        }
-        // f[x] = min{f[x-coins[i]]+1};
-        for (int i = 0; i < f.length; i++) {
-            //当前金额无法兑换到，所以不能进行下一步
-            if (f[i] == 0) {
-                continue;
-            }
-            for (int j = 0; j < coins.length; j++) {
-                //提交后发现测试用例里有个int边界值，需要排除
-                if ((long) i + (long) coins[j] > Integer.MAX_VALUE) {
-                    continue;
-                }
-                //当前金额 加上一个硬币的金额 必须小于等于 目标金额amount
-                if (i + coins[j] <= amount) {
-                    if (f[i + coins[j]] == 0 || f[i] + 1 < f[i + coins[j]]) {
-                        f[i + coins[j]] = f[i] + 1;
-                    }
+        f[0] = 0;
+        // 状态转移方程 f[x] = f[x-i]+1
+        for (int i = 1; i < amount; i++) {
+            // 整个状态下的最优解
+            int min = Integer.MAX_VALUE;
+            // 遍历所有可能的状态
+            for (int j = 0; j < n; j++) {
+                // 当前的硬币可以组成amount则进行更新
+                if (i >= coins[j] && f[i - coins[j]] < min) {
+                    min = f[i - coins[j]] + 1;
                 }
             }
+            // 保存结果
+            f[i] = min;
         }
-        return f[amount] == 0 ? -1 : f[amount];
+        return f[amount] == Integer.MAX_VALUE ? -1 : f[amount];
+
     }
 
     public static void main(String[] args) {
-        int[] coins = new int[]{2,5,7};
+        int[] coins = new int[]{2, 5, 7};
         int amount = 27;
-        System.out.println(coinChange(coins,amount));
+        System.out.println(coinChange(coins, amount));
     }
 }
