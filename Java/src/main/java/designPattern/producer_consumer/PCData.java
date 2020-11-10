6a202029eb271683d98c4f21dc033d2e461bb364
heyou-macbook <heyou_0423@163.com>
2020-11-10 23:56:45 +0800

    add:生产者与消费者的共享数据模型

---
+++ Java/src/main/java/designPattern/producer_consumer/PCData.java
@@ -0,0 +1,23 @@
+package designPattern.producer_consumer;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2020/11/10 23:53
+ */
+public class PCData {
+    private final int intData;
+    public PCData(int d){
+        intData = d;
+    }
+
+    public PCData(String d){
+        intData = Integer.valueOf(d);
+    }
+
+    @Override
+    public String toString() {
+        return "PCData{" +
+                "intData=" + intData +
+                '}';
+    }
+}
