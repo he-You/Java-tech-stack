3482addf4bbf0326a61180298bc78aee55cf101f
heyou-macbook <heyou_0423@163.com>
2020-11-12 23:43:43 +0800

    add:disruptor-数据模型

---
+++ Java/src/main/java/designPattern/disruptor/PCData.java
@@ -0,0 +1,19 @@
+package designPattern.disruptor;
+
+/**
+ * 数据模型
+ *
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2020/11/10 23:53
+ */
+public class PCData {
+    private long value;
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+}
