c67c8808c6f38fde853bcf5a06ebe6e7ca9d771d
heyou-macbook <heyou_0423@163.com>
2020-11-21 23:03:30 +0800

    add:奇偶交换并行实现

--- Java/src/main/java/concurrent/ParallelOddEvenSort.java
+++ Java/src/main/java/concurrent/ParallelOddEvenSort.java
@@ -11,11 +11,14 @@ import java.util.concurrent.Executors;
  * @date 2020/11/21 22:25
  */
 public class ParallelOddEvenSort {
-    static int[] arr= {25,48,65,87,123,233,456,666,777,8999,55555};
+    static int[] arr = {25, 48, 65, 87, 123, 233, 456, 666, 777, 8999, 55555};
 
     static ExecutorService pool = Executors.newCachedThreadPool();
 
-    static int exchFlag = 1;	//记录当前迭代是否发生了数据交换
+    /**
+     * 记录当前迭代是否发生了数据交换
+     */
+    static int exchFlag = 1;
 
     public static synchronized int getExchFlag() {
         return exchFlag;
@@ -26,23 +29,25 @@ public class ParallelOddEvenSort {
     }
 
     public static void pOddEvenSort(int[] arr) throws InterruptedException {
-        int start = 0;			//记录交换类型。0为偶交换，1为奇交换
+        //记录交换类型。0为偶交换，1为奇交换
+        int start = 0;
 
         //如果上次比较发生了数据交换，或者当前正在进行奇交换，循环不会停止；
         //直到程序不再发生交换，或者当前进行的是偶交换，即奇偶交换已经成对出现。
-        while(getExchFlag() == 1 || start ==1) {
+        while (getExchFlag() == 1 || start == 1) {
             setExchFlag(0);
             //偶数的数值长度，当start=1时，只有len/2-1个线程。倒计时线程数。
-            CountDownLatch latch = new CountDownLatch(arr.length/2 -(arr.length%2 == 0 ? start :0));
-            for(int i =start; i < arr.length-1; i+=2) {
-                pool.submit(new OddEvenSortTask(i,latch));
+            CountDownLatch latch = new CountDownLatch(arr.length / 2 - (arr.length % 2 == 0 ? start : 0));
+            for (int i = start; i < arr.length - 1; i += 2) {
+                pool.submit(new OddEvenSortTask(i, latch));
             }
-            latch.await();//等待所有线程结束
-            start = start ==0 ? 1 : 0;
+            //等待所有线程结束
+            latch.await();
+            start = start == 0 ? 1 : 0;
         }
     }
 
-    static class OddEvenSortTask implements Runnable{
+    static class OddEvenSortTask implements Runnable {
         int i;
         CountDownLatch latch;
 
@@ -54,10 +59,10 @@ public class ParallelOddEvenSort {
 
         @Override
         public void run() {
-            if(arr[i] > arr[i+1]) {
+            if (arr[i] > arr[i + 1]) {
                 int temp = arr[i];
-                arr[i]   = arr[i+1];
-                arr[i+1] = temp;
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
                 setExchFlag(1); //数据进行了交换
             }
             latch.countDown();//结束当前线程的任务，倒计时器-1
@@ -67,7 +72,7 @@ public class ParallelOddEvenSort {
     public static void main(String[] args) throws InterruptedException {
         pOddEvenSort(arr);
         for (int i : arr) {
-            System.out.print(i+" ");
+            System.out.print(i + " ");
         }
     }
 }
