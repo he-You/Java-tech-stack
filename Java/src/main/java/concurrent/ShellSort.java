c160bd47a5f07f8ddabd85d3e3af32fab3bc5d9c
heyou-macbook <heyou_0423@163.com>
2020-11-26 21:31:55 +0800

    add:希尔排序的并行实现

--- Java/src/main/java/concurrent/ShellSort.java
+++ Java/src/main/java/concurrent/ShellSort.java
@@ -1,5 +1,9 @@
 package concurrent;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * 希尔排序：将整个数组根据时间间隔 h分割为若干个子数组，他们相互穿插在一起，
  * 每次排序时分别对每个子数组进行排序，每次排序时总是交换间隔为 h的两个元素；
@@ -9,6 +13,9 @@ package concurrent;
  * @date 2020/11/24 23:19
  */
 public class ShellSort {
+    static int[] arr = {25, 48, 65, 87, 123, 233, 456, 666, 777, 8999, 55555};
+    static ExecutorService pool = Executors.newCachedThreadPool();
+
     // 串行实现
     public static void shellSort(int[] arr) {
         // 计算出最大的 h值
@@ -34,10 +41,71 @@ public class ShellSort {
         }
     }
 
+    // 并行实现
+    public static class ShellSortTask implements Runnable {
+
+        int i = 0;
+        int h = 0;
+        CountDownLatch latch;
+
+        public ShellSortTask(int i, int h, CountDownLatch countDownLatch) {
+            this.i = i;
+            this.h = h;
+            this.latch = countDownLatch;
+        }
+
+        @Override
+        public void run() {
+            if (arr[i] < arr[i - h]) {
+                int temp = arr[i];
+                int j = i - h;
+                while (j >= 0 && arr[j] > temp) {
+                    arr[j + h] = arr[j];
+                    j -= h;
+                }
+                arr[j + h] = temp;
+            }
+            latch.countDown();
+        }
+    }
+
+    public static void pShellSort() throws InterruptedException {
+        // 计算出最大的 h值
+        int h = 1;
+        CountDownLatch countDownLatch = null;
+        while (h < arr.length / 3) {
+            h = h * 3 + 1;
+        }
+        while (h > 0) {
+            System.out.println("h=" + h);
+            if (h >= 4) {
+                countDownLatch = new CountDownLatch(arr.length - h);
+            }
+            for (int i = h; i < arr.length; i++) {
+                // 控制线程数量
+                if (h > 4) {
+                    pool.execute(new ShellSortTask(i, h, countDownLatch));
+                } else {
+                    if (arr[i] < arr[i - h]) {
+                        int temp = arr[i];
+                        int j = i - h;
+                        while (j >= 0 && arr[j] > temp) {
+                            arr[j + h] = arr[j];
+                            j -= h;
+                        }
+                        arr[j + h] = temp;
+                    }
+                }
+            }
+        }
+        countDownLatch.await();
+        h = (h - 1) / 3;
+    }
+
     public static void main(String[] args) {
         int[] arr = {25, 48, 65, 87, 123, 233, 456, 666, 777, 8999, 55555};
         shellSort(arr);
-        for (int i:arr) {
+        for (int i : arr) {
             System.out.println(i);
         }
     }
