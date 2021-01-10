cb845568c3a2517119a4775de0c72f3deda74114
heyou-macbook <heyou_0423@163.com>
2021-01-10 15:25:27 +0800

    add:通过继承 Thread 的匿名内部类创建线程

---
+++ Java/src/main/java/concurrent/thread/CreatedThreadByExtendsAnonymousInnerClass.java
@@ -0,0 +1,18 @@
+package concurrent.thread;
+
+/**
+ * 通过继承 Thread匿名内部类创建线程
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2021/1/10 下午2:41
+ */
+public class CreatedThreadByExtendsAnonymousInnerClass {
+    public static void main(String[] args) {
+        // 初始化线程实例
+        new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + " is running");
+            }
+        }.start();
+    }
+}
