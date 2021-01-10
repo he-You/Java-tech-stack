b823cea140048edc88177cd3029751479aa6b59f
heyou-macbook <heyou_0423@163.com>
2021-01-10 15:25:40 +0800

    add:通过继承 Thread 创建线程

---
+++ Java/src/main/java/concurrent/thread/CreatedThreadByExtendsThread.java
@@ -0,0 +1,20 @@
+package concurrent.thread;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2021/1/10 下午2:37
+ */
+public class CreatedThreadByExtendsThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println(getName() + " is running");
+    }
+
+    public static void main(String[] args) {
+        new CreatedThreadByExtendsThread().start();
+        new CreatedThreadByExtendsThread().start();
+        new CreatedThreadByExtendsThread().start();
+        new CreatedThreadByExtendsThread().start();
+    }
+}
