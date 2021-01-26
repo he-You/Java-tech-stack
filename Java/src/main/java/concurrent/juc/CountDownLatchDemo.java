9acc291d1fdf2932c43eb042ad6dce372c331ce7
heyou-macbook <heyou_0423@163.com>
2021-01-26 23:47:01 +0800

    add:倒计数器 CountDownLatch

---
+++ Java/src/main/java/concurrent/juc/CountDownLatchDemo.java
@@ -0,0 +1,37 @@
+package concurrent.juc;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2021/1/26 下午10:56
+ */
+public class CountDownLatchDemo implements Runnable {
+    static final CountDownLatch end = new CountDownLatch(10);
+
+    static final CountDownLatchDemo demo = new CountDownLatchDemo();
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(new Random().nextInt(10)*1000);
+            System.out.println("check complete");
+            end.countDown();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(demo);
+        }
+        end.await();;
+        System.out.println("start");
+        executorService.shutdown();
+    }
+}
