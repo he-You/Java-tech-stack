34adb5b05be3df66a5233d02fdf00bbacb464001
heyou-macbook <heyou_0423@163.com>
2021-01-31 23:36:08 +0800

    add:CyclicBarrier 线程池使用时Demo

--- Java/src/main/java/concurrent/juc/CyclicBarrierDemo.java
+++ Java/src/main/java/concurrent/juc/CyclicBarrierDemo.java
@@ -10,29 +10,28 @@ import java.util.concurrent.Executors;
  * @date 2021/1/31 下午5:33
  */
 public class CyclicBarrierDemo {
-    // 创建一个 CyclicBarrier实例，添加一个所有子线程全部到达屏障后执行的任务
-    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(2, new Runnable() {
-        @Override
-        public void run() {
-            System.out.println(Thread.currentThread() + "task1 merge result");
-        }
+    /**
+     * 创建一个 CyclicBarrier实例，添加一个所有子线程全部到达屏障后执行的任务
+     *
+     * @param parties:计数器的初始值
+     * @param Runnable:是当计数器为 0 时需要执行的任务
+     */
+    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(2, () -> {
+        System.out.println(Thread.currentThread() + "task1 merge result");
     });
 
     public static void main(String[] args) {
         // 创建一个线程个数固定为 2 的线程池
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         // 将线程 A添加到线程池
-        executorService.submit(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    System.out.println(Thread.currentThread() + "task1-1");
-                    System.out.println(Thread.currentThread() + "enter in barrier");
-                    cyclicBarrier.await();
-                    System.out.println(Thread.currentThread() + "enter out barrier");
-                } catch (InterruptedException | BrokenBarrierException e) {
-                    e.printStackTrace();
-                }
+        executorService.submit(() -> {
+            try {
+                System.out.println(Thread.currentThread() + "task1-1");
+                System.out.println(Thread.currentThread() + "enter in barrier");
+                cyclicBarrier.await();
+                System.out.println(Thread.currentThread() + "enter out barrier");
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
             }
         });
         // 将线程 B添加到线程池
