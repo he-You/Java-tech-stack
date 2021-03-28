84a2c164e35531256420a25816c3d704d6177bc7
heyou-macbook <heyou_0423@163.com>
2021-03-28 21:29:09 +0800

    add:静态常量

---
+++ MessageQuene/src/main/java/com/heyou/msgquene/rabbitmq/RabbitConstant.java
@@ -0,0 +1,13 @@
+package com.heyou.msgquene.rabbitmq;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2021/3/28 下午4:55
+ */
+public interface RabbitConstant {
+    String EXCHANGE_WEATHER = "exchange_weather";
+    String EXCHANGE_WEATHER_ROUTING = "exchange_weather_routing";
+    String EXCHANGE_WEATHER_TOPIC = "exchange_weather_topic";
+    String QUEUE_SINA = "queue_sina";
+    String QUEUE_BAIDU = "queue_baidu";
+}
