ea597998db2d6eea5de716492fb593f1c1c0dc52
heyou-macbook <heyou_0423@163.com>
2021-03-29 22:11:18 +0800

    add:生产者Confirm、Return 机制监听消息投递状态

---
+++ MessageQuene/src/main/java/com/heyou/msgquene/rabbitmq/confirm/SinaWeather.java
@@ -0,0 +1,41 @@
+package com.heyou.msgquene.rabbitmq.confirm;
+
+import com.heyou.msgquene.rabbitmq.ConnectionUtils;
+import com.heyou.msgquene.rabbitmq.RabbitConstant;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2021/3/28 下午4:57
+ */
+public class SinaWeather {
+    public static void main(String[] args) throws IOException {
+        // 获取 TCP长连接
+        Connection connection = ConnectionUtils.getConnection();
+        // 获取虚拟连接
+        Channel channel = connection.createChannel();
+        // 声明队列
+        //第一个参数：队列名称ID
+        //第二个参数：是否持久化，false对应不持久化数据，MQ停掉数据就会丢失
+        //第三个参数：是否队列私有化，false则代表所有消费者都可以访问，true代表只有第一次拥有它的消费者才能一直使用，其他消费者不让访问
+        //第四个：是否自动删除,false代表连接停掉后不自动删除掉这个队列
+        //其他额外的参数, null
+        channel.queueDeclare(RabbitConstant.QUEUE_SINA, false, false, false, null);
+        //queueBind用于将队列与交换机绑定
+        //参数1：队列名 参数2：交互机名  参数三：Routingkey
+        //指定队列与交换机以及routing key之间的关系
+        channel.queueBind(RabbitConstant.QUEUE_SINA, RabbitConstant.EXCHANGE_WEATHER_TOPIC, "us.#");
+        // basicQos,MQ不再对消费者一次发送多个请求，而是消费者处理完一个消息后（确认后），在从队列中获取一个新的
+        channel.basicQos(1);
+
+        channel.basicConsume(RabbitConstant.QUEUE_SINA , false , new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("新浪天气收到气象信息：" + new String(body));
+                channel.basicAck(envelope.getDeliveryTag() , false);
+            }
+        });
+    }
+}
---
+++ MessageQuene/src/main/java/com/heyou/msgquene/rabbitmq/confirm/WheatherCenter.java
@@ -0,0 +1,67 @@
+package com.heyou.msgquene.rabbitmq.confirm;
+
+import com.heyou.msgquene.rabbitmq.ConnectionUtils;
+import com.heyou.msgquene.rabbitmq.RabbitConstant;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2021/3/28 下午4:54
+ */
+public class WheatherCenter {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = ConnectionUtils.getConnection();
+        Channel channel = connection.createChannel();
+        // 开启监听模式
+        channel.confirmSelect();
+        channel.addConfirmListener(new ConfirmListener() {
+            @Override
+            public void handleAck(long l, boolean b) throws IOException {
+                //第二个参数代表接收的数据是否为批量接收，一般我们用不到。
+                System.out.println("消息已被Broker接收,Tag:" + l );
+            }
+
+            @Override
+            public void handleNack(long l, boolean b) throws IOException {
+                System.out.println("消息已被Broker拒收,Tag:" + l);
+            }
+        });
+        channel.addReturnListener(new ReturnCallback() {
+            @Override
+            public void handle(Return r) {
+                System.err.println("===========================");
+                System.err.println("Return编码：" + r.getReplyCode() + "-Return描述:" + r.getReplyText());
+                System.err.println("交换机:" + r.getExchange() + "-路由key:" + r.getRoutingKey() );
+                System.err.println("Return主题：" + new String(r.getBody()));
+                System.err.println("===========================");
+            }
+        });
+        Map<String, String> data = getWeatherData();
+        for (Map.Entry<String, String> itemData : data.entrySet()) {
+            //第一个参数交换机名字   其他参数和之前的一样
+            // 第二个参数表示消息的routingkey
+            channel.basicPublish(RabbitConstant.EXCHANGE_WEATHER_TOPIC, itemData.getKey(), null, itemData.getValue().getBytes());
+        }
+//        channel.close();
+//        connection.close();
+    }
+
+    public static Map<String, String> getWeatherData() {
+        Map<String, String> area = new LinkedHashMap<>();
+        area.put("china.hunan.changsha.20201127", "中国湖南长沙20201127天气数据");
+        area.put("china.hubei.wuhan.20201127", "中国湖北武汉20201127天气数据");
+        area.put("china.hunan.zhuzhou.20201127", "中国湖南株洲20201127天气数据");
+        area.put("us.cal.lsj.20201127", "美国加州洛杉矶20201127天气数据");
+
+        area.put("china.hebei.shijiazhuang.20201128", "中国河北石家庄20201128天气数据");
+        area.put("china.hubei.wuhan.20201128", "中国湖北武汉20201128天气数据");
+        area.put("china.henan.zhengzhou.20201128", "中国河南郑州20201128天气数据");
+        area.put("us.cal.lsj.20201128", "美国加州洛杉矶20201128天气数据");
+        return area;
+    }
+}
